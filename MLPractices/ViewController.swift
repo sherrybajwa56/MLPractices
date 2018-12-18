1a59cffb7129cf7da9c50bea8504a7217d4a1341
Sherry <sherry.bajwa@idc.bedbath.com>
2018-12-18 13:39:23 +0530

    Initial Commit

---
+++ MLPractices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MLPractices
+//
+//  Created by Sherry.Bajwa on 18/12/18.
+//  Copyright © 2018 Sherry.Bajwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
